e5659c17ddb3ad0f939c8c7ce533f96102b5d734
Aliaksandr Piatkevich <aliaksandr.piatkevich@rbi-group-it.com>
2026-05-07 14:53:56 +0200

    change config

--- src/main/java/at/abcdef/memmaster/config/DatabaseConfiguration.java
+++ src/main/java/at/abcdef/memmaster/config/DatabaseConfiguration.java
@@ -5,8 +5,6 @@ import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,17 +13,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories("at.abcdef.memmaster.repository")
 @EnableTransactionManagement
-@EnableConfigurationProperties(FlywayProperties.class)
 public class DatabaseConfiguration {
 
    private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
    @Bean(initMethod = "migrate")
-   public Flyway flyway(DataSource dataSource, FlywayProperties flywayProperties) {
-      log.info("Configuring Flyway with locations: {}", flywayProperties.getLocations());
+   public Flyway flyway(DataSource dataSource) {
+      log.info("Configuring Flyway migration...");
       return Flyway.configure()
               .dataSource(dataSource)
-              .locations(flywayProperties.getLocations().toArray(new String[0]))
+              .locations("classpath:/db/migration")
               .baselineOnMigrate(true)
               .load();
    }
