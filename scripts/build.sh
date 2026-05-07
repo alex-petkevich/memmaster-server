d54b078f371860568013aa15342bd84c389aef5c
alex <s@homesite.by>
2026-05-07 11:26:21 +0200

    change configs

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+chown memmaster:memmaster ./target/memmaster-server-1.0.0-SNAPSHOT.jar
+chmod u+x ./target/memmaster-server-1.0.0-SNAPSHOT.jar
+systemctl stop memmaster
+sleep 3
+cp ./target/memmaster-server-1.0.0-SNAPSHOT.jar /mnt/vol1/www/memmaster-server/memmaster-server.jar
+chown memmaster:memmaster /mnt/vol1/www/joplin/memmaster-server.jar
+systemctl start memmaster
