09b687f05e699e6834376a70e2ae397ca1d99aab
Aliaksandr Piatkevich <aliaksandr.piatkevich@rbi-group-it.com>
2026-05-07 14:44:19 +0200

    change config

--- src/main/java/at/abcdef/memmaster/config/DatabaseConfiguration.java
+++ src/main/java/at/abcdef/memmaster/config/DatabaseConfiguration.java
@@ -1,5 +1,13 @@
 package at.abcdef.memmaster.config;
 
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -7,14 +15,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories("at.abcdef.memmaster.repository")
 @EnableTransactionManagement
+@EnableConfigurationProperties(FlywayProperties.class)
 public class DatabaseConfiguration {
-   /*@Bean
-   public FlywayMigrationStrategy repairFlyway() {
-      return flyway -> {
-         // repair each script's checksum
-         flyway.repair();
-         // before new migrations are executed
-         flyway.migrate();
-      };
-   }*/
+
+   private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
+   @Bean(initMethod = "migrate")
+   public Flyway flyway(DataSource dataSource, FlywayProperties flywayProperties) {
+      log.info("Configuring Flyway with locations: {}", flywayProperties.getLocations());
+      return Flyway.configure()
+              .dataSource(dataSource)
+              .locations(flywayProperties.getLocations().toArray(new String[0]))
+              .baselineOnMigrate(true)
+              .load();
+   }
 }
